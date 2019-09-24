447a6ff2c04bb34ef2940ecbb6794f27f629e359
Tim de Klijn <timdeklijn@gmail.com>
2019-09-24 15:18:56 +0200

    Added comments to nn

--- flappy_fish/nn.py
+++ flappy_fish/nn.py
@@ -10,7 +10,11 @@ class NN():
 
 
     def create_NN(self):
-        """Create neural network"""
+        """Create neural network
+
+        Create and compile a model and save it in:
+        self.model
+        """
         self.model = Sequential()
         # outputs a layer with 7*[21] weights
         self.model.add(Dense(8, input_dim=4, activation="relu"))
@@ -23,6 +27,11 @@ class NN():
 
 
     def mutate(self, weights_list):
+        """
+        Mutate weights.
+
+        Returns a weight list.
+        """
         chance = 0.05
         mutate_range = 0.1
         new_weights_list = []
@@ -37,21 +46,38 @@ class NN():
 
 
     def change_weights(self, weights):
-       for i, layer in enumerate(self.model.layers):
+        """Replace weights in self.model with weights"""
+        for i, layer in enumerate(self.model.layers):
             layer.set_weights(weights[i])
 
 
     def next_generation(self):
+        """
+        Create the next generation by:
+            * finding the fittest fish in self.dead (list of fish)
+            * Extract the weights of the fish that need to reproduce
+            * Mix/Mutate the weights and set weights to next generation
+            * For all fish, run fish_reset()
+            * Append all changed fish to self.alive (list of fish)
+            * empty self.dead (list of fish)
+        """
+
+        # Sort fish on score
         self.dead.sort(key=lambda b: b.score, reverse=True)
+        # Extract all weights from the fittest fish
         weights_list = []
         weights_list = [np.array(l.get_weights()) for l in self.dead[0].model.layers]
+        # Let the fittest fish live, and reset it
         self.alive.append(self.dead[0])
         self.max_score = self.alive[0].score
         self.alive[0].reset_fish()
+        # Mutate the weights of the fittest fish and spread over the population
         for b in self.dead[1:]:
             fish_weights_list = self.mutate(weights_list)
             b.change_weights(fish_weights_list)
+            # Reset all fish
             b.reset_fish()
             self.alive.append(b)
+        # update dead list
         self.dead = []
 
